7e636a9cc699ca5831c4a384c0afec3b6362a0fd
Adrien SÉON <adrien.seon@gmail.com>
2019-07-15 02:22:58 +0200

    Create LightsScreen.js

---
+++ screens/LightsScreen.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {View, StyleSheet} from 'react-native';
+import Colors from '../constants/Colors';
+import MenuIcon from '../components/icons/Menu';
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: Colors.appBackground,
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	menuBtn: {
+		marginLeft: 7.5
+	}
+});
+
+class LightsScreen extends React.Component {
+	static navigationOptions = {
+		title: 'Luminaires',
+		headerStyle: {
+			height: 56,
+		},
+		headerTransparent: true,
+		headerTintColor: Colors.primaryText,
+		headerTitleStyle: {
+			fontWeight: '600',
+		},
+		headerLeft: <MenuIcon style={styles.menuBtn} color={Colors.primaryText} size='32'/>
+	};
+
+	render() {
+		return (
+			<View style={styles.container}>
+
+			</View>
+		);
+	}
+}
+
+export default LightsScreen;
