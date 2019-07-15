722486ee6825018d8f61b668eaf0af7fa904156e
Adrien SÉON <adrien.seon@gmail.com>
2019-07-15 02:22:24 +0200

    Create Back.js

---
+++ components/icons/Back.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Svg, { Rect } from 'react-native-svg'
+
+const Back = props => (
+  <Svg width={props.size} height={props.size} fill="none" {...props}>
+    <Path
+      d="M20.75 5L10 15.75 20.75 26.5"
+      stroke={props.color}
+      strokeWidth={2}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </Svg>
+)
+
+export default Back
