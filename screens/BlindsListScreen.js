7ef889c925e3becf74a2e8fabf53ecad59fd2bdb
Adrien SÉON <adrien.seon@gmail.com>
2019-07-17 01:53:20 +0200

    Update BlindsListScreen.js

--- screens/BlindsListScreen.js
+++ screens/BlindsListScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {Platform, View, Text, StyleSheet, StatusBar} from 'react-native';
 import Colors from '../constants/Colors';
 import BackIcon from '../components/icons/Back';
 
@@ -10,12 +10,13 @@ const styles = StyleSheet.create({
 		justifyContent: "center",
 		alignItems: "center",
 	},
-	backBtn: {
-		marginLeft: 7.5
-	}
 });
 
 class BlindsListScreen extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+	
 	static navigationOptions = {
 		title: 'Stores',
 		headerStyle: {
@@ -26,8 +27,21 @@ class BlindsListScreen extends React.Component {
 		headerTitleStyle: {
 			fontWeight: '600',
 		},
-		headerBackImage: <BackIcon style={styles.backBtn} color={Colors.primaryText} size='32'/>,
+		headerBackImage: <BackIcon color={Colors.primaryText} size='32'/>,
 	};
+
+	componentDidMount() {
+		this._navListener = this.props.navigation.addListener('didFocus', () => {
+			StatusBar.setBarStyle('light-content');
+			if(Platform.OS === "android"){
+				StatusBar.setBackgroundColor('#6a51ae');
+			}
+		});
+	}
+
+	componentWillUnmount() {
+		this._navListener.remove();
+	}
 
 	render() {
 		return (
