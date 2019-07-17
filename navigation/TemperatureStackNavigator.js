a776a8e685c846c5faffa11006df7d57a68e984b
Adrien SÉON <adrien.seon@gmail.com>
2019-07-18 00:41:35 +0200

    Update TemperatureStackNavigator.js

--- navigation/TemperatureStackNavigator.js
+++ navigation/TemperatureStackNavigator.js
@@ -4,17 +4,30 @@ import TemperatureIcon from '../components/icons/Temperature';
 import TemperatureScreen from '../screens/TemperatureScreen';
 import Colors from '../constants/Colors';
 
-const TemperatureStackNavigator = createStackNavigator(
-	{
-		Temperature: TemperatureScreen,
+const TemperatureStackNavigator = createStackNavigator({
+	Temperature: TemperatureScreen,
+},{
+	initialRouteName: 'Temperature',
+	defaultNavigationOptions: {
+		headerStyle: {
+			height: 56,
+		},
+		headerTransparent: true,
+		headerTintColor: Colors.primaryText,
+		headerTitleStyle: {
+			fontFamily: "OpenSans-Semibold",
+			fontSize: 18,
+		},
+		headerBackTitle: null,
+		headerTruncatedBackTitle: null,
 	}
-);
+});
 
 TemperatureStackNavigator.navigationOptions = {
 	tabBarLabel: 'Temperature',
 	tabBarIcon: ({ focused }) => (
 		<TemperatureIcon color={focused ? Colors.primaryBrand : Colors.secondaryBrand} size='32' />
-	),
+	)
 };
 
 TemperatureStackNavigator.path = '';
