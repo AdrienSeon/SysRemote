b61aed457a18bb265e378dd3cbb2272655b1500a
Adrien SÉON <adrien.seon@gmail.com>
2019-07-18 00:41:15 +0200

    Create User.js

---
+++ components/icons/User.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import {TouchableHighlight} from 'react-native';
+import Colors from '../../constants/Colors';
+import Svg, { Path, Circle } from 'react-native-svg'
+
+class User extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+			<TouchableHighlight underlayColor={Colors.appBackground} {...this.props}>
+				<Svg width={this.props.size} height={this.props.size} fill="none">
+					<Path d="M14.5 15H.5s.538-6.5 7-6.5c6.461 0 7 6.5 7 6.5z" fill={this.props.color} />
+					<Circle cx={7.5} cy={3.75} fill={this.props.color} fillOpacity={0.5} r={3.75} />
+				</Svg>
+			</TouchableHighlight>
+		);
+	}
+}
+
+export default User;
