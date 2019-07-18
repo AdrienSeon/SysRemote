6cdf1502ac4c69a6db2b02df5dba64d76955c974
Adrien SÉON <adrien.seon@gmail.com>
2019-07-19 01:51:08 +0200

    Update AppNavigator.js

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,6 +1,24 @@
 import React from 'react';
-import { createAppContainer } from 'react-navigation';
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+//import { Transition } from 'react-native-reanimated';
 
+import AuthLoadingScreen from '../screens/AuthLoadingScreen';
 import DrawerNavigator from './DrawerNavigator';
+import AuthStackNavigator from './AuthStackNavigator';
 
-export default createAppContainer(DrawerNavigator);
+export default createAppContainer(createSwitchNavigator(
+  {
+    AuthLoading: AuthLoadingScreen,
+    Drawer: DrawerNavigator,
+    Auth: AuthStackNavigator,
+  },
+  {
+    initialRouteName: 'Auth',
+/*	transition: (
+		<Transition.Together>
+			<Transition.Out	type="slide-bottom"	durationMs={400} interpolation="easeIn" />
+			<Transition.In type="fade" durationMs={500} />
+		</Transition.Together>
+	),*/
+  }
+));
