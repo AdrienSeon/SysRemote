d74b2a9c879e0f793983f2fbaa6afdeae2bdad71
Adrien SÉON <adrien.seon@gmail.com>
2019-07-19 01:51:21 +0200

    Update DrawerScreen.js

--- screens/DrawerScreen.js
+++ screens/DrawerScreen.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import {Platform, SafeAreaView, View, Text, Button, StyleSheet, StatusBar, Image} from 'react-native';
+import {Platform, SafeAreaView, View, Text, Button, StyleSheet, StatusBar, Image, TouchableNativeFeedback, TouchableOpacity, AsyncStorage } from 'react-native';
 import { DrawerItems } from 'react-navigation';
 import Colors from '../constants/Colors';
 import AppConfig from '../constants/AppConfig';
-import MenuIcon from '../components/icons/Menu';
+import SettingsIcon from '../components/icons/Settings';
+import LogoutIcon from '../components/icons/Logout';
 import { LinearGradient } from 'expo-linear-gradient';
 
 const styles = StyleSheet.create({
@@ -12,8 +13,9 @@ const styles = StyleSheet.create({
 	},
 	linearGradient: {
 		flex: 1,
+		justifyContent: 'space-between',
 	},
-	title: {
+	titleContainer: {
 		flex: 1,
 		marginTop: 80,
 		alignItems: 'center',
@@ -45,7 +47,27 @@ const styles = StyleSheet.create({
 		color: Colors.inverted
 	},
 	menu: {
+		flex: 1,
+		justifyContent: 'flex-end',
 		marginBottom: 40,
+		alignItems: 'stretch',
+	},
+	menuItem: {
+		flexDirection: 'row',
+		height: 45,
+		alignItems: 'center',
+	},
+	menuItemLabel: {
+		flex: 18,
+		fontFamily: "OpenSans",
+		fontSize: 14,
+		lineHeight: 27,
+		color: Colors.inverted,
+		marginLeft: 10,
+	},
+	MenuItemIcon: {
+		flex: 2,
+		marginLeft: 40,
 	},
 });
 
@@ -71,18 +93,37 @@ class DrawerScreen extends React.Component {
 	componentWillUnmount() {
 		//this._navListener.remove();
 	}
-	
+
+	LogoutAsync = async () => {
+		await AsyncStorage.clear();
+		this.props.navigation.navigate('Auth');
+	};
+
 	render() {
+
+		let TouchablePlatformSpecific = Platform.OS === 'ios' ? TouchableOpacity : TouchableNativeFeedback;
+
 		return (
 			<SafeAreaView style={styles.container}>
 				<LinearGradient colors={['rgba(149, 161, 183, 1)', 'rgba(172, 186, 211, 1)']} start={{ x: 1, y: 1 }} end={{ x: 0, y: 0}} style={styles.linearGradient}>
-					<View style={styles.title}>
+					<View style={styles.titleContainer}>
 						<Image style={styles.logo}/>
 						<Text style={styles.appName}>{AppConfig.appName}</Text>
 						<Text style={styles.buildingName}>{AppConfig.buildingName}</Text>
 					</View>
 					<View style={styles.menu}>
-						<DrawerItems {...this.props}/>
+						<TouchablePlatformSpecific onPress={() => this.props.navigation.navigate('Settings')}>
+							<View style={styles.menuItem}>
+								<SettingsIcon style={styles.MenuItemIcon} color={Colors.inverted} size='21'/>
+								<Text style={styles.menuItemLabel}>Paramètres</Text>
+							</View>
+						</TouchablePlatformSpecific>
+						<TouchablePlatformSpecific onPress={this.LogoutAsync}>
+							<View style={styles.menuItem}>
+								<SettingsIcon style={styles.MenuItemIcon} color={Colors.inverted} size='21'/>
+								<Text style={styles.menuItemLabel}>Se déconnecter</Text>
+							</View>
+						</TouchablePlatformSpecific>
 					</View>
 				</LinearGradient>
 			</SafeAreaView>
