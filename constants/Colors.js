0bb626bd305ca1ea596ba09917a783adef6aef20
Adrien SÉON <adrien.seon@gmail.com>
2019-07-20 04:14:18 +0200

    Update Colors.js

--- constants/Colors.js
+++ constants/Colors.js
@@ -1,28 +1,16 @@
-const tintColor = '#2f95dc';
-
 export default {
-  tintColor,
-  tabIconDefault: '#ccc',
-  tabIconSelected: tintColor,
-  tabBar: '#fefefe',
-  errorBackground: 'red',
-  errorText: '#fff',
-  warningBackground: '#EAEB5E',
-  warningText: '#666804',
-  noticeBackground: tintColor,
-  noticeText: '#fff',
-
-  primaryBrand: "rgb(71, 138, 247)",
-  appBackground: "rgb(239, 242, 246)",
-  secondaryBrand: "rgb(195, 205, 223)",
-  primaryBrand50: "rgba(71, 138, 247, 0.5)",
-  secondaryBrandColor50: "rgba(195, 205, 223, 0.5)",
-  primaryText: "rgb(47, 60, 78)",
-  secondaryText: "rgb(145, 157, 180)",
-  primaryBrandGradientDark: "rgb(52, 115, 244)",
-  primaryBrandGradientLight: "rgb(90, 162, 251)",
-  inverted: "rgb(255, 255, 255)",
-  inverted50: "rgba(255, 255, 255, 0.5)",
-  tertiaryBrand: "rgb(255, 220, 133)",
-  inverted: "rgb(255, 255, 255)"
+    primaryBrand: "rgb(71, 138, 247)",
+    appBackground: "rgb(239, 242, 246)",
+    secondaryBrand: "rgb(195, 205, 223)",
+    primaryBrand50: "rgba(71, 138, 247, 0.5)",
+    secondaryBrandColor50: "rgba(195, 205, 223, 0.5)",
+    primaryText: "rgb(47, 60, 78)",
+    secondaryText: "rgb(145, 157, 180)",
+    tertiaryText: "rgb(195, 205, 223)",
+    primaryBrandGradientDark: "rgb(52, 115, 244)",
+    primaryBrandGradientLight: "rgb(90, 162, 251)",
+    inverted: "rgb(255, 255, 255)",
+    inverted50: "rgba(255, 255, 255, 0.5)",
+    tertiaryBrand: "rgb(255, 220, 133)",
+    inverted: "rgb(255, 255, 255)"
 };
