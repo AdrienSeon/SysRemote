ff8d29e7793ae021582c6db1a23a31b173176061
Adrien SÉON <adrien.seon@gmail.com>
2019-07-20 19:45:47 +0200

    Update App.js

--- App.js
+++ App.js
@@ -3,8 +3,6 @@ import { Asset } from 'expo-asset';
 import * as Font from 'expo-font';
 import React, { useState } from 'react';
 import { Platform, StatusBar, StyleSheet, View } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
-
 import AppNavigator from './navigation/AppNavigator';
 
 export default function App(props) {
@@ -35,11 +33,6 @@ async function loadResourcesAsync() {
       require('./assets/images/robot-prod.png'),
     ]),
     Font.loadAsync({
-      // This is the font that we are using for our tab bar
-      ...Ionicons.font,
-      // We include SpaceMono because we use it in HomeScreen.js. Feel free to
-      // remove this if you are not using it in your app
-      'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
       'OpenSans': require('./assets/fonts/OpenSans-Regular.ttf'),
       'OpenSans-Semibold': require('./assets/fonts/OpenSans-Semibold.ttf'),
     }),
