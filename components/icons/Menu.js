ccfd14be994618043089bf211fa0c354ddb4f595
Adrien SÉON <adrien.seon@gmail.com>
2019-07-21 02:22:47 +0200

    Update Menu.js

--- components/icons/Menu.js
+++ components/icons/Menu.js
@@ -11,7 +11,7 @@ class Menu extends React.Component {
 	render() {
 		return (
 			<TouchableHighlight underlayColor={Colors.appBackground} {...this.props}>
-				<Svg width={this.props.size} height={this.props.size} fill="none">
+				<Svg width={this.props.size} height={this.props.size} viewBox="0 0 32 32" fill="none">
 					<Rect x={3} y={4} width={15} height={2.573} rx={1} fill={this.props.color} />
 					<Rect x={3} y={14.937} width={25} height={2.573} rx={1} fill={this.props.color} />
 					<Rect x={3} y={25.873} width={21.25} height={2.573} rx={1} fill={this.props.color} />
