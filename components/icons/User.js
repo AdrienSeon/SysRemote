a3464966207855aa4b5265e03acdc92c9eda48cb
Adrien SÉON <adrien.seon@gmail.com>
2019-07-21 02:23:43 +0200

    Update User.js

--- components/icons/User.js
+++ components/icons/User.js
@@ -11,7 +11,7 @@ class User extends React.Component {
 	render() {
 		return (
 			<TouchableHighlight underlayColor={Colors.appBackground} {...this.props}>
-				<Svg width={this.props.size} height={this.props.size} fill="none">
+				<Svg width={this.props.size} height={this.props.size} viewBox="0 0 15 15" fill="none">
 					<Path d="M14.5 15H.5s.538-6.5 7-6.5c6.461 0 7 6.5 7 6.5z" fill={this.props.color} />
 					<Circle cx={7.5} cy={3.75} fill={this.props.color} fillOpacity={0.5} r={3.75} />
 				</Svg>
