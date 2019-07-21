11496448f8978ab216efad05a9ae4310fdbecbca
Adrien SÉON <adrien.seon@gmail.com>
2019-07-21 02:23:25 +0200

    Update Lights.js

--- components/icons/Lights.js
+++ components/icons/Lights.js
@@ -11,13 +11,16 @@ class Lights extends React.Component {
 	render() {
 		return (
 			<TouchableHighlight underlayColor={Colors.appBackground} {...this.props}>
-				<Svg width={this.props.size} height={this.props.size} fill="none">
+				<Svg width={this.props.size} height={this.props.size} viewBox="0 0 32 32" fill="none">
 					<Path
 						d="M15.5 27C10 27 10 22 10 22h11s0 5-5.5 5zM14.5 4a1 1 0 1 1 2 0v7h-2V4z"
 						fill={this.props.color}
 						fillOpacity={0.5}
 					/>
-					<Path d="M27 21H4s.523-11 11.5-11S27 21 27 21z" fill={this.props.color} />
+					<Path
+						d="M27 21H4s.523-11 11.5-11S27 21 27 21z"
+						fill={this.props.color}
+					/>
 				</Svg>
 			</TouchableHighlight>
 		);
