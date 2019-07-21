5a0badd2087f1996b374a2460005dce92eb53292
Adrien SÉON <adrien.seon@gmail.com>
2019-07-21 02:23:34 +0200

    Update Back.js

--- components/icons/Back.js
+++ components/icons/Back.js
@@ -11,7 +11,7 @@ class Back extends React.Component {
 	render() {
 		return (
 			<TouchableHighlight underlayColor={Colors.appBackground} {...this.props}>
-				<Svg width={this.props.size} height={this.props.size} fill="none">
+				<Svg width={this.props.size} height={this.props.size} viewBox="0 0 32 32" fill="none">
 					<Path
 						d="M20.75 5L10 15.75 20.75 26.5"
 						stroke={this.props.color}
