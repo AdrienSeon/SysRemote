72a6e6f481ff98d805a94b0c76e832c3aef7b67e
Adrien SÉON <adrien.seon@gmail.com>
2019-07-26 02:06:18 +0200

    Update LightsListScreen.js

--- screens/LightsListScreen.js
+++ screens/LightsListScreen.js
@@ -1,23 +1,41 @@
-import React from 'react';
-import {Platform, View, Text, StyleSheet, StatusBar} from 'react-native';
+import React, { Component } from 'react';
+import {Platform, SafeAreaView, ScrollView, View, Text, StyleSheet, StatusBar} from 'react-native';
 import Colors from '../constants/Colors';
 import BackIcon from '../components/icons/Back';
+import SingleLightCommand from '../components/SingleLightCommand';
+import { Header } from 'react-navigation';
 
 const styles = StyleSheet.create({
-	container: {
+	safearea: {
 		flex: 1,
 		backgroundColor: Colors.appBackground,
-		justifyContent: "center",
-		alignItems: "center",
+	},
+	container: {
+		flex: 1,
+		paddingTop:  Platform.OS === 'ios' ? Header.HEIGHT - 10 : Header.HEIGHT + 25,
 	},
 	backBtn: {
 		marginLeft: Platform.OS === 'ios' ? 10 : 0,
+	},
+	singleCommand: {
+
 	}
 });
 
-class LightsListScreen extends React.Component {
+class LightsListScreen extends Component {
 	constructor(props) {
 		super(props);
+
+		this.state = {
+			light1Selected: false,
+			light2Selected: false,
+			light3Selected: false,
+			light4Selected: false,
+			light5Selected: false,
+			light6Selected: false,
+			light7Selected: false,
+			light8Selected: false,
+		};
 	}
 
 	static navigationOptions = {
@@ -40,9 +58,68 @@ class LightsListScreen extends React.Component {
 
 	render() {
 		return (
-			<View style={styles.container}>
-				<Text style={styles.baseText}>Liste des luminaires</Text>
-			</View>
+			<SafeAreaView style={styles.safearea}>
+				<View style={styles.container}>
+					<ScrollView>
+						<View style={{flex: 1, flexDirection: 'row',}}>
+							<SingleLightCommand
+								name='Luminaire 1'
+								selected={this.state.light1Selected}
+								onChange={value => this.setState({light1Selected: value})}
+								style={styles.singleCommand}
+							/>
+							<SingleLightCommand
+								name='Luminaire 2'
+								selected={this.state.light2Selected}
+								onChange={value => this.setState({light2Selected: value})}
+								style={styles.singleCommand}
+							/>
+						</View>
+						<View style={{flex: 1, flexDirection: 'row',}}>
+							<SingleLightCommand
+								name='Luminaire 3'
+								selected={this.state.light3Selected}
+								onChange={value => this.setState({light3Selected: value})}
+								style={styles.singleCommand}
+							/>
+							<SingleLightCommand
+								name='Luminaire 4'
+								selected={this.state.light4Selected}
+								onChange={value => this.setState({light4Selected: value})}
+								style={styles.singleCommand}
+							/>
+						</View>
+						<View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-around'}}>
+							<SingleLightCommand
+								name='Luminaire 5'
+								selected={this.state.light5Selected}
+								onChange={value => this.setState({light5Selected: value})}
+								style={styles.singleCommand}
+							/>
+							<SingleLightCommand
+								name='Luminaire 6'
+								selected={this.state.light6Selected}
+								onChange={value => this.setState({light6Selected: value})}
+								style={styles.singleCommand}
+							/>
+						</View>
+						<View style={{flex: 1, flexDirection: 'row',}}>
+							<SingleLightCommand
+								name='Luminaire 7'
+								selected={this.state.light7Selected}
+								onChange={value => this.setState({light7Selected: value})}
+								style={styles.singleCommand}
+							/>
+							<SingleLightCommand
+								name='Luminaire 8'
+								selected={this.state.light8Selected}
+								onChange={value => this.setState({light8Selected: value})}
+								style={styles.singleCommand}
+							/>
+						</View>
+					</ScrollView>
+				</View>
+			</SafeAreaView>
 		);
 	}
 }
