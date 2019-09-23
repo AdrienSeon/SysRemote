667d73b76fd584471620bf3be102992a76f3b77c
Adrien SÉON <adrien.seon@gmail.com>
2019-09-23 19:46:28 +0200

    Update AppConfig.js

--- src/constants/AppConfig.js
+++ src/constants/AppConfig.js
@@ -2,7 +2,8 @@ export default {
 	appName: 'SysRemote',
 	buildingName: 'Nom du bâtiment',
 	device: {
-		host: '192.168.1.18',
+		//host: '192.168.1.18',
+		host: '86.202.21.19',
 		username: 'admin',
 		password: 'Syscom8556'
 	}
